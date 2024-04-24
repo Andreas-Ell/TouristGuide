139a14309ddb450c6e7f991b46c92c8ec6df02b7
Andreas-Ell <anel0002@stud.kea.dk>
2024-04-24 18:36:06 +0200

    final

--- src/main/java/com/example/touristguide/model/TouristTAG.java
+++ src/main/java/com/example/touristguide/model/TouristTAG.java
@@ -2,18 +2,51 @@ package com.example.touristguide.model;
 
 public class TouristTAG {
 
-    private String tag;
+    private int tagID;
+    private String tagNameOne;
+    private String tagNameTwo;
+    private String tagNameThree;
 
-    public TouristTAG(String tag) {
-        this.tag = tag;
+    public TouristTAG(int tagID, String tagNameOne, String tagNameTwo, String tagNameThree) {
+        this.tagID = tagID;
+        this.tagNameOne = tagNameOne;
+        this.tagNameTwo = tagNameTwo;
+        this.tagNameThree = tagNameThree;
     }
 
-    public String getTag() {
-        return tag;
+
+    public int getTagID() {
+        return tagID;
     }
 
-    public void setTag(String newTag) {
-        tag = newTag;
+
+    public String getTagNameOne() {
+        return tagNameOne;
+    }
+
+
+    public String getTagNameTwo() {
+        return tagNameTwo;
+    }
+
+
+    public String getTagNameThree() {
+        return tagNameThree;
+    }
+
+
+    public void setTagNameOne(String newTagNameOne) {
+        tagNameOne = newTagNameOne;
+    }
+
+
+    public void setTagNameTwo(String newTagNameTwo) {
+        tagNameTwo = newTagNameTwo;
+    }
+
+
+    public void setTagNameThree(String newTagNameThree) {
+        tagNameThree = newTagNameThree;
     }
 
 }
